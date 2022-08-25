a2e75df7e984ab1a80094f05f3f709c372691690
DezzlesS <vadimbannikov98@gmail.com>
2022-08-25 23:35:40 +0600

    Игра сделана за исключением мелких штрихов

--- constants.js
+++ constants.js
@@ -4,6 +4,29 @@ export const
         [[1,3],[4,6]], [[4,6],[4,6]], [[7,9],[4,6]],
         [[1,3],[7,9]], [[4,6],[7,9]], [[7,9],[7,9]]
     ],
+    findBox = (x, y) => (
+        (x <=3 && y <= 3) ? 0 :
+        (x <=6 && y <= 3) ? 1 :
+        (x <=9 && y <= 3) ? 2 :
+        (x <=3 && y <= 6) ? 3 :
+        (x <=6 && y <= 6) ? 4 :
+        (x <=9 && y <= 6) ? 5 :
+        (x <=3 && y <= 9) ? 6 : 
+        (x <=6 && y <= 9) ? 7 :
+                            8
+    ),
     digits = [1,2,3,4,5,6,7,8,9],
-    emptyGrid = () => [...Array(9)].map(() => Array(9).fill(0))
+    gridPattern = () => [...Array(9)].map(() => Array(9).fill(0))
 ;
+
+export function collectRevealedCells(cells) {
+    const pattern = Object.fromEntries(
+        [1,2,3,4,5,6,7,8,9].map(d => [d, new Set()])
+    )
+
+    cells.forEach(cell => {
+        const digit = cell.innerText;
+        if (digit) pattern[digit].add(cell)
+    });
+    return pattern;
+}
--- js.js
+++ js.js
@@ -1,12 +1,13 @@
-import { boxesSchem, digits, emptyGrid } from "./constants.js";
+import { boxesSchem, collectRevealedCells, digits, gridPattern, findBox } from "./constants.js";
 
-const { random, floor } = Math;
+const { random, floor, round } = Math;
 
 function domAssemble(main) {
     this.field = $('#field');
     this.cells = $('.cell')
         .attr('data-index', i => i)
-        .on('mousedown', (e) => main.cellClick(e))
+        .on('mousedown', (e) => { main.cellClick(e) })
+        .toArray()
     ;
     this.rows = [...$('.row')].map(row => [...row.children]);
 
@@ -15,112 +16,166 @@ function domAssemble(main) {
     this.boxes = boxesSchem.map(([
         [col1,col2],
         [row1,row2]
-    ]) => [...this.cells].filter(({dataset}) => (
+    ]) => this.cells.filter(({dataset}) => (
             (dataset.col >= col1 && dataset.col <= col2) &&
             (dataset.row >= row1 && dataset.row <= row2)
         ))
     );
+
+    $(document).on('keydown', ({key}) => {
+        switch (key) {
+            case 'Backspace': main.cellClear();    break;
+            default         : main.cellInput(key); break;
+        }
+    })
 }
 
 class Sudoku {
     constructor() {
         Object.assign(this, new domAssemble(this))
-        this.gridCount = 0;
+        let gridCount = 0;
 
-        do this.grid = this.generateGrid()
-        while (this.grid === undefined);
+        do    {this.correctGrid = this.generateGrid(); gridCount++}
+        while (this.correctGrid === undefined);
 
-        this.values = this.grid.flat();
-        this.hideDigits();
+        
+        this.correctValues = this.correctGrid.flat();
+
+        this.playGrid = this.generatePlayGrid();
         this.displayGrid();
-        console.log(this.gridCount);
 
+        this.revealedCells = collectRevealedCells(this.cells);
 
-        $(document).on('keydown', ({ key }) => {
-            if (/[0-9]/.test(key)) this.digitInput(key);
-        })
-    }
-
-    sameDigitsHighlight() {
-
-    }
-
-    findBox(x, y) {
-        const index = 
-            (x <=3 && y <= 3) ? 0 :
-            (x <=6 && y <= 3) ? 1 :
-            (x <=9 && y <= 3) ? 2 :
-            (x <=3 && y <= 6) ? 3 :
-            (x <=6 && y <= 6) ? 4 :
-            (x <=9 && y <= 6) ? 5 :
-            (x <=3 && y <= 9) ? 6 : 
-            (x <=6 && y <= 9) ? 7 :
-                                8
-        ;
-        return this.boxes[index];
-    }
-
-    digitInput(key) {
-        this.target.text(key)
+        console.log(gridCount);
     }
 
     cellClick({target}) {
-        this.removeCellHighlight();
+        if (
+            this.target?.data('index') ==
+            target.dataset.index
+        ) return;
+
+        this.removeAdjacentsHighlight();
         this.target = $(target);
-        const col = this.target.data('col') - 1,
-              row = this.target.data('row') - 1
-        ;
-        this.setCellHighlight(col, row);
-
-        if (target.innerText) sameDigitsHighlight(col, row);
+        this.targetCoordinates = this.getTargetCoordinates();
+        this.setAdjacentsHighlight();
+        
+        this.removeSimilarsHighlight();
+        if (target.innerText) {
+            this.pickedDigit = target.innerText;
+            this.setSimilarsHighlight();
+        }
     }
 
-    removeCellHighlight() {
-        this.highlightItems?.removeClass('--adjacent');
-        this.target?.removeClass('--active');
+    cellInput(key) {
+        if (
+            !this.target?.attr('input') ||
+            this.target?.text() === key ||
+            !/[0-9]/.test(key)
+        ) return;
+        
+        this.target.text(key);
+
+        this.removeSimilar();
+        this.lastInput = key;
+        
+        this.addToPlayGrid();
+
+        this.addSimilar();
+        this.removeSimilarsHighlight();
+        this.setSimilarsHighlight();
+
+
+        this.completeCheck();
+    }
+
+    cellClear() {
+        if (
+            !this.target.attr('input') ||
+            !this.target.text()
+        ) return;
+
+        
+        this.target.text('');
+        this.removeSimilar();
+        this.removeSimilarsHighlight();
+        
+        this.deleteFromPlayGrid();
     }
 
 
-    setCellHighlight(col, row) {
+
+    setAdjacentsHighlight(
+        [col, row] = this.targetCoordinates
+    ) {
         this.highlightItems = $([
-            ...this.findBox(col+1, row+1),
+            ...this.boxes[findBox(col+1, row+1)],
             ...this.columns[col],
             ...this.rows[row]
-        ]).addClass('--adjacent')
+        ]).addClass('--adjacent');
 
         this.target
             .removeClass('--adjacent')
             .addClass('--active')
         ;
     }
-
-
-    hideDigits() {
-        const { length } = this.values;
-        const indexes = [...Array(length)]
-            .map((_,i) => i)
-            .sort(() => random() - 0.5)
-            .slice(length / 2)
-        ;
-        this.displayValues = this.values;
-        indexes.forEach(v => this.values[v] = '');
+    removeAdjacentsHighlight() {
+        this.highlightItems?.removeClass('--adjacent');
+        this.target?.removeClass('--active');
     }
-    
+
+
+    setSimilarsHighlight() {
+        this.similars = $([...this.revealedCells[this.pickedDigit]]);
+        this.similars.addClass('--similar');
+        console.log(this.similars);
+        console.log(this.pickedDigit);
+    }
+    removeSimilarsHighlight() {
+        this.similars?.removeClass('--similar');
+        this.similars = $();
+    }
+
+
+    addSimilar() {
+        this.pickedDigit = this.lastInput;
+        this.revealedCells[this.pickedDigit].add(this.target[0]);
+    }
+    removeSimilar() {
+        this.revealedCells[this.lastInput]?.delete(this.target[0]);
+        this.target.removeClass('--similar');
+    }
+
+
+    generatePlayGrid() {
+        this.displayValues = this.correctValues.map(
+            v => round(random())
+                ? v
+                : ''
+        );
+        const playGrid = [...Array(9)].map((_,i) => this.displayValues.slice(i*9, i*9+9)); 
+        return playGrid;
+    }
 
     displayGrid() {
-        this.cells.text(i => this.displayValues[i])
+        this.cells.forEach((cell, i) => {
+            cell.InnerText = this.displayValues[i];
+        });
+        this.displayValues.forEach((v, i) => {
+            if (v) this.cells[i].innerText = v;
+            else   this.cells[i].setAttribute('input', true)
+        })
     }
 
     generateGrid() {
-        const grid = emptyGrid();
-        const { boxes } = this;
+        const grid = gridPattern();
 
         for (const digit of digits) {
             const 
                 columns = [],
                 rows = []
             ;
-            for (const box of boxes) {
+            for (const box of this.boxes) {
                 const 
                     affordableCells = box.filter(({dataset}) => {
                         const
@@ -136,7 +191,7 @@ class Sudoku {
 
                     randomCell = pickRandomCell(affordableCells)
                 ;
-                if (!randomCell) { this.gridCount++; return };
+                if (!randomCell) return;
                 
                 const
                     col = randomCell.dataset.col - 1,
@@ -159,6 +214,53 @@ class Sudoku {
             return affordable[index];
         }
     }
+
+    addToPlayGrid() {
+        const [col, row] = this.targetCoordinates;
+
+        this.playGrid[row][col] = this.lastInput;
+    }
+    deleteFromPlayGrid() {
+        const [col, row] = this.targetCoordinates;
+
+        this.playGrid[row][col] = '';
+    }
+
+    completeCheck() {
+        this.puzzleCompleted = this.correctGrid.every((row1, i) => {
+            const row2 = this.playGrid[i];
+            return row1 === row2;
+        })
+        if (!this.puzzleCompleted) return;
+        
+        this.resetGame();
+        this.congratilations();
+    }
+
+    congratilations() {
+        alert('Вы решили судоку. Примите поздравления от разработчика!')
+    }
+
+
+    resetGame() {
+        // this.cells.off('mousedown');
+        // $(document).off('keydown');
+
+        this.removeAdjacentsHighlight();
+        this.removeSimilarsHighlight();
+
+        $(this.cells)
+        //    .removeClass('--adjacent --active --similar')
+           .removeAttr('input')
+        ;
+    }
+    
+
+
+    getTargetCoordinates = () => [
+        this.target.data('col') - 1,
+        this.target.data('row') - 1
+    ]
 }
 
 
